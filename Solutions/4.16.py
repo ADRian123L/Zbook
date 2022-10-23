658788644a5853a7c904f4dd5106dc47d7dce9ac
ADRian123L <adrian163@usf.edu>
2022-10-23 19:20:33 -0400

    Solved.

---
+++ Solutions/4.16.py
@@ -0,0 +1,18 @@
+# 4.16.py  Author: Adrian
+
+# The program pomps for an integer and resturns the reverse of it's
+# corresponding binary number.
+
+# The function resturns the binary number in reverse:
+def binary(input) -> int:
+    bin = ""
+    # Converts from decimal to reverse binary.
+    while (input >= 1): 
+        calculation = (input % 2)
+        bin += str(calculation)
+        input //= 2
+    return (bin)
+
+# Calls and outputs the result:
+integer = int(input())
+print(binary(integer))    
