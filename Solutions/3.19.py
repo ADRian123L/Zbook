35ffa35ed905f65ff8b325d83e82090441fb781a
ADRian123L <adrian163@usf.edu>
2022-10-22 23:48:14 -0400

    Complted the code.

--- Solutions/3.19.py
+++ Solutions/3.19.py
@@ -1,6 +1,20 @@
 # The program promps the user for information and outputs an appropiate answer.
 
+# Promps for the type of service:
 auto_service = input("Enter desired auto service:\n")
 
-print(f"You entered: {auto_service}")
+# Creates a dictionary for storing the service's cost:
+services = {
+
+	"Oil change" : "$35",
+	"Tire rotation" : "$19",
+	"Car wash" : "$7"
+}
+
+for key, cost in services.items():
+	if (auto_service == key):
+		print(f"Cost of {key.lower()}: {cost}")
+		break
+
+
 
