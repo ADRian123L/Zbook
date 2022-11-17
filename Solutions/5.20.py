c832d23e8e7197a7335b7df7f983483cea61c2e0
ADRian123L <adrian163@usf.edu>
2022-11-17 14:58:43 -0500

    Finished problem v1.

---
+++ Solutions/5.20.py
@@ -0,0 +1,28 @@
+# 5.20.py  Author: Adrian
+
+# The program prompts for the miles walked and returns the number of
+# steps taken.
+
+# The function:
+def feet_to_steeps(feet : float) -> int:
+    """The function takes the an amount in foots and returns the number 
+    of steps take."""
+    steps = feet / 2.5
+    steps = int(steps) 
+    return steps
+
+def reed() -> float:
+    """The function prompts for the number of foots and returns
+    the input as a floating-point number."""
+    feet = float(input())
+    return feet
+
+def main() -> None:
+    """The function runs the program."""
+    feet = reed()
+    steps = feet_to_steeps(feet)
+    print(steps)
+    return None
+
+if __name__ == "__main__":
+    main()
