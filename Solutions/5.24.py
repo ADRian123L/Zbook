65060209e55d11c1ecbd0155563a818115156953
ADRian123L <adrian163@usf.edu>
2022-11-29 13:23:51 -0500

    Added comments to the finished problem.

--- Solutions/5.24.py
+++ Solutions/5.24.py
@@ -7,27 +7,35 @@
 def jiffies_to_seconds(jiffy : float) -> float:
     """The functions takes a number in jiffy and returns 
     it seconds."""
+    # Convert from jiffy to seconds:
     seconds : float = float(jiffy / 100)
+    # Round to three decimal places:
     seconds : float = round(seconds, 3)
     return seconds
 
 def main(run=False) -> None:
     """The function drives the program."""
     if (run == True):
+        # Calls a function get the jiffy:
         jiffy : float = get()
+        # Converts the jiffy into seconds:
         seconds : float = jiffies_to_seconds(jiffy)
+        # Prints the seconds:
         out(seconds)
     return None
 
 def out(seconds : float) -> None:
     """The function prints the result."""
+    # Prints the seconds:
     print(seconds)
     return None
 
 def get() -> float:
     """The function prompts and return a jiffy."""
+    # Prompts for the jiffy:
     jiffy : float = float(input())
     return jiffy
 
 if __name__ == "__main__":
+    # Runs the program:
     main(run=True)
